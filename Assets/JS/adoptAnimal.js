905fe5740b7115d74873759b677500032ac311f0
mathiasmundt99 <mathiasbeck99@gmail.com>
2024-09-19 14:31:46 +0200

    JS Comments

--- Assets/JS/adoptAnimal.js
+++ Assets/JS/adoptAnimal.js
@@ -1,7 +1,14 @@
+//https://www.w3schools.com/howto/howto_js_dropdown.asp
 
+// Først bruges querySelectorAll til at ramme alle elementer med klassen dropdownBtn
+//Her påsættes der en forEach, som itterer igennem alle elementerne 
 document.querySelectorAll('.dropdownBtn').forEach(button => {
+
+// Her tilføjes en EventListner på elementerne, som lytter efter 'click', og derefter eksekvere en funktion
     button.addEventListener('click', function(event) {
+// Her bruges this til at refferer til elementet som blev klikket på. this.nextElemtSibling sikre at det næste elemnt altid er det næste i rækken ift HTML
         const dropdownOptions = this.nextElementSibling;
+// Her bruges en if- else til at sætte displey = block/none alt efter deres current state
         if (dropdownOptions.style.display === 'block') {
             dropdownOptions.style.display = 'none';
         } else {
@@ -10,7 +17,8 @@ document.querySelectorAll('.dropdownBtn').forEach(button => {
     });
 });
 
-// W3 School 
+// https://www.w3schools.com/howto/howto_js_dropdown.asp
+// Funktion som sikre at klik udenfor boksen lukker dropdown ned
 window.onclick = function(event) {
     if (!event.target.matches('.dropdownBtn') && !event.target.closest('.dropdownOptions')) {
         const dropdowns = document.querySelectorAll('.dropdownOptions');
